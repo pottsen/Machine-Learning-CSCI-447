ae70e98278852414775514fc3f702520d32aac2a
BruceClark406 <brucewestonmt@gmail.com>
2019-09-16 16:04:04 -0600

    Data Processing commit 1

---
+++ Assignment_2/Real_to_Discrete.py
@@ -0,0 +1,10 @@
+#abalone        
+#class:         [0]
+#Real           [1,2,3,4,5,6,7]
+#Descrete       [8]
+
+
+#car        
+#class:         [6]
+#Real           []
+#Descrete       [0,1,2,3,4,5]     
