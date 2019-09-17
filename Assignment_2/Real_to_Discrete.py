3d49e512e064046bb2e53727baf07a0626077513
BruceClark406 <brucewestonmt@gmail.com>
2019-09-17 09:35:11 -0600

    data cleaning with pandas

--- Assignment_2/Real_to_Discrete.py
+++ Assignment_2/Real_to_Discrete.py
@@ -30,4 +30,23 @@
 
 import pandas as pd
 
-df = pd.read_csv("./data/wine.csv") 
+df = pd.read_csv("./data/wine.csv", index_col=0) 
+
+dataDesc = df.describe()
+
+print(dataDesc.info())
+print(dataDesc.iloc[4][0])
+
+print(dataDesc)
+
+col = (df[14.23])
+
+
+#pd.cut(df[0], bins=[0,13,15])
+
+#, labels=["0-25", "25-50", "50-75", "75-100"]
+#dataDesc.iloc[3][0], dataDesc.iloc[4][0], dataDesc.iloc[5][0], dataDesc.iloc[6][0], dataDesc.iloc[7][0]
+
+#print(df.describe())
+
+#print(dataDesc.iloc[4][0])
