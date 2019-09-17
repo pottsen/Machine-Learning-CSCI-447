ba45aac5eeedb5833007fffd1ebd012650004789
BruceClark406 <brucewestonmt@gmail.com>
2019-09-17 17:40:43 -0600

    cleaning data with pandas

--- Assignment_2/Real_to_Discrete.py
+++ Assignment_2/Real_to_Discrete.py
@@ -31,21 +31,41 @@
 import pandas as pd
 
 df = pd.read_csv("./data/wine.csv", index_col=0) 
+column_names = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
+df.columns = column_names
+
+
 
 dataDesc = df.describe()
 
-print(dataDesc.info())
-print(dataDesc.iloc[4][0])
 
-print(dataDesc)
+for i in range(len(column_names)):
+    new_col = pd.cut(df[column_names[i]], bins=[dataDesc.iloc[3][i], dataDesc.iloc[4][i], dataDesc.iloc[5][i], dataDesc.iloc[6][i], dataDesc.iloc[7][i]], labels=["1", "2", "3", "4"])
+    df[column_names[i]] = new_col
 
-col = (df[14.23])
+df.to_csv("./processed/wine_discrete.csv", sep=",", index=True, header=False)
+#write data frame out to new file
+
+#columns={1,2,3,4,5,6,7,8,9,10,11,12,13,14}
+#
 
 
-#pd.cut(df[0], bins=[0,13,15])
 
-#, labels=["0-25", "25-50", "50-75", "75-100"]
-#dataDesc.iloc[3][0], dataDesc.iloc[4][0], dataDesc.iloc[5][0], dataDesc.iloc[6][0], dataDesc.iloc[7][0]
+
+#print(dataDesc.head())
+#print(dataDesc.info())
+#print(dataDesc.iloc[4][0])
+
+#print(dataDesc)
+
+#col = (df["14.23"])
+#print(col)
+
+
+
+
+
+
 
 #print(df.describe())
 
