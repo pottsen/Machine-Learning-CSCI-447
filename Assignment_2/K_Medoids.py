f566525c702a59fa6aac4bf2ac1d41fe9343cf33
pottsen <peter.k.ottsen@gmail.com>
2019-10-08 13:24:57 -0600

    saving progress on medoids. updated like 16 of Main to have sections

--- Assignment_2/K_Medoids.py
+++ Assignment_2/K_Medoids.py
@@ -3,7 +3,6 @@ from Main import shuffle_pd_df, slicer, slice_pd_df_using_np
 
 def k_medoids(k, dataframes):    
     #pull k random data points from training data to be medoids
-    
     #randomize the training data
     shuffled_training = shuffle_pd_df(dataframes)
     #slice into sections for medoids and training set
@@ -13,28 +12,88 @@ def k_medoids(k, dataframes):
     #set training data
     training_data = shuffled_sliced_training[1:]
 
+    
+    
+
     runFull = True
-    while runFull = True:
+    while runFull == True:
         #for left over data points associate each to the closest medoid by using distance
-        totalCost = 0.0
-        medoidList[]
+        medoid_dictionary = {}
         for index, row in training_data.iterrows():
             #find the closest medoid
             closest_medoid = nearest_k_points(1, medoids, row)
-            #append medoid index to training datum
-            training_data.append[closest_medoid[0][0]]
-            #store lists of data points assigned to that medoid
-            
-            #dictionary of medoids and associated points
+            closest_medoid_index = closest_medoid[0][0]
+            #store lists of data points assigned to that medoid in a dictionary
+            try: #add traning data
+                medoid_dataset = medoid_dictionary[closest_medoid_index] 
+                medoid_dictionary[closest_medoid_index].update(medoid_dataset, index)
+
+            except:
+                medoid_dictionary.update({closest_medoid_index:index})
+
+        #Swap to false so it wil not be rerun unnless a medoid is swapped below
+        runFull = False
+
+        for key in medoid_dictionary:
+            #initialize minimum cost to large value
+            minimum_cost = 1000000000000000
+
+            #include medoid in the cluster
+            cluster_points = medoids[key]
+
+            #add points mapped to medoid to cluster
+            for value in medoid_dictionary[key]:
+                for index in training_data:
+                    if value == index:
+                        cluster_points = cluster_points.append(training_data[index])
+
+            #calculate cost of the medoid
+            # medoid_distances = nearest_k_points(len(cluster_points),cluster_points, medoids[key])
+            # for point in medoid_distances:
+            #         minumum_cost = minimum_cost + medoid_distances[test_point][1]
+
+            # cluster_points = cluster_points.append(medoids[key])
+
+            #calculate cost of all points in cluster and update medoid if cost is less than medoid cost
+            #have counter k=0 since first point will be medoid
+            k=0
+            for test_point in cluster_points:
+                #resets cost for each point in cluster
+                cost = 0
+
+                #returns array of the points with the distances to each from from the test point
+                all_point_distance_array = nearest_k_points(len(cluster_points)-1,cluster_points, test_point)
+
+                #add up costs to get total
+                for point in all_point_distance_array:
+                    cost = cost + all_point_distance_array[test_point][1]
+                
+                #if new cost is less than medoid cost or previous, update
+                if cost < minimum_cost and k > 0:
+                    minimum_cost = cost
+                    #will need to rerun full medoid if a point is swapped
+                    runFull = True
+
+                    #swap out medoied with data point that has lower cost
+                    temp_medoid = medoids[key]
+                    medoids[key] = test_point
+                    training_data[test_point] = temp_medoid
+
+                k = k + 1
+
+             
+
+
+
         
-        #check all points associated to that medoid to see if one has a lower cost than the current medoid
+        # #check all points associated to that medoid to see if one has a lower cost than the current medoid
 
-        #if a point has lower cost than medoid,then update that to be the medoid
+        # #if a point has lower cost than medoid,then update that to be the medoid
 
-        #if a point was swapped with medoid rerun initial total assignment and then recheck if any assigned points are better
-            totalCost = totalCost + closest_medoid[0][1]
-            medoidList[closest_medoid[0][0]].add
-            #do we need to check if classes line up?
+        # #if a point was swapped with medoid rerun initial total assignment and then recheck if any assigned points are better
+        #     totalCost = totalCost + closest_medoid[0][1]
+        #     medoidList[closest_medoid[0][0]].add
+        #     #do we need to check if classes line up?
 
 
 
