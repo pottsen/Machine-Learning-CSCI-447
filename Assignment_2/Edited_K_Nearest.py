d7ec944163d5ba299319595f8165c6c5157c3f28
BruceClark406 <brucewestonmt@gmail.com>
2019-10-09 15:33:39 -0600

    Update Edited_K_Nearest.py

--- Assignment_2/Edited_K_Nearest.py
+++ Assignment_2/Edited_K_Nearest.py
@@ -8,20 +8,19 @@ def edited_k_nearest(k, training_data):
     for index, row in training_data.iterrows():
         k_closest = nearest_k_points(k+1, training_data, row)
         k_closest = k_closest[1:]
+        
         guesses = []
-
         for j in range(k):
             guesses.append(k_closest[j][-1])
-        
         guess = max(set(guesses), key = guesses.count) 
 
         #determine actual class
         actual_class = row[0]
 
         if actual_class != guess:
-            training_data.drop(index)
+            training_data = training_data.drop(index)
+            #print(index , " dropped")
+        # else:
+        #     print(index , " kept")
             
     return training_data   
-
-
-# return 
