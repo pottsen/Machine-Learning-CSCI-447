b930351f56297a46670d2b7a43c49e1f2101ef74
BruceClark406 <brucewestonmt@gmail.com>
2019-10-10 19:21:23 -0600

    new commit

--- Assignment_2/Edited_K_Nearest.py
+++ Assignment_2/Edited_K_Nearest.py
@@ -10,27 +10,28 @@ def edited_k_nearest(k, training_data):
         pre_triaining_data = training_data
 
         for index, row in training_data.iterrows():
-            k_closest = nearest_k_points(k+1, training_data, row)
+            print(k)
+            k_closest = nearest_k_points(k, training_data, row)
             k_closest = k_closest[1:]
-            
+            print(k_closest)
+            print("our traing")
             guesses = []
             for j in range(len(k_closest)):
                 guesses.append(k_closest[j][-1])
-           
-            print(guesses)
+            
             guess = max(set(guesses), key = guesses.count) 
 
             #determine actual class
             actual_class = row[0]
 
             if actual_class != guess:
-                training_data = training_data.drop(index)
-                print("size of training set " , len(training_data))
+                training_data.drop(index)
+                
                 #print(index , " dropped")
             # else:
             #     print(index , " kept")
+            if (len(training_data) <= k):
+                return training_data
         if (len(training_data)) == (len(pre_triaining_data)):
-            print(training_data)
             return training_data
-    print(training_data)
     return training_data   
