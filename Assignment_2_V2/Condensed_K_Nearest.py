39f4ae3fb295debf23eb188787aa174bb5e95bcc
Forest Edwards <35545924+ForestSkis98@users.noreply.github.com>
2019-10-21 20:33:43 -0600

    Fixed a minor error

--- Assignment_2_V2/Condensed_K_Nearest.py
+++ Assignment_2_V2/Condensed_K_Nearest.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import random
-from K_NearestNeighbor import k_Nearest_Points
+from K_NearestNeighbor import k_Nearest_Points, classification_guess
 
 def condensed_k_nearest(k, training_data):
     random.shuffle(training_data)
@@ -13,12 +13,7 @@ def condensed_k_nearest(k, training_data):
 
         for i in range(len(training_data)): # Takes each item in the training data, and finds the nearest neighbor in the training data
             nearest = k_Nearest_Points(k, training_data, training_data[i]) # Finds k nearest neighbors 
-            guesses = [] # stores the class of each of the nearest points
-
-            for j in range(len(nearest)):
-                guesses.append(nearest[j][-1]) # append class of each point in nearest
-                
-            guess = max(set(guesses), key = guesses.count) # Finds the highest occurring class in the nearest k points.
+            guess = classification_guess(nearest)
             if training_data[i][0] != guess and not df_row_in_list(training_data[i], condensed): #Is the data point actual class different from the guessed class? and is the point not already contained in condensed?
                 condensed.append(training_data[i]) # 
                 new_data_point = True # New datapoint added to condensed, continue while loop
