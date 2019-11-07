f265f5e9d045e33874dee73c6193fcf6998186b3
pottsen <peter.k.ottsen@gmail.com>
2019-11-07 11:01:53 -0700

    made clean code files

---
+++ Assignment_3_V2/MLP_clean.py
@@ -0,0 +1,154 @@
+import numpy as np
+
+class MLP2():
+    #input-> #data_as_2dList, possible_outputs, number_of_hidden_layers, number_of_hidden_nodes_in_each_layer, momentum(T/F)
+    def __init__(self, data, output, number_of_layers, number_of_nodes, momentum):
+         
+        ### Initialize variables
+        # flag
+        self.momentum = bool(momentum)
+        # learning rates
+        self.momentum_factor = 0.1
+        self.learing_rate = 0.05
+
+        # for use in making data structures
+        self.output_size = output
+        self.data = data
+        self.number_of_layers = 2 + int(number_of_layers)
+        self.number_of_weight_matrices = self.number_of_layers -1
+
+        # tracking vairables
+        self.outputs = np.zeros(len(output))
+        self.outputs.shape = (1,len(output))
+
+        # check for correct inputs
+        if (len(number_of_nodes) != number_of_layers):
+                raise Exception ("we need to know how many nodes are in each hidden layer")
+
+        #make a layers list of np arrays. input is first layer and output is last layer
+        self.layers = []
+        for layer in range(int(self.number_of_layers)):
+            if layer == 0:
+                self.layers.append(np.zeros((len(data[0])-1,1)))
+            elif layer == int(self.number_of_layers)-1:
+                self.layers.append(np.zeros((len(output), 1)))
+            else:
+                self.layers.append(np.zeros((number_of_nodes[layer-1], 1)))
+            print("Layer Shape\n", self.layers[layer].shape)
+
+        #make an list of wieght matricies and previous deltas for momentum
+        self.weight_matricies = []
+        self.previous_WM_delta = []
+        for i in range(int(self.number_of_layers)-1):
+            x ,y = self.layers[i].shape
+            x2 ,y2 = self.layers[i+1].shape
+            self.weight_matricies.append(np.random.rand(x2, x))
+            self.previous_WM_delta.append(np.zeros((x2, x)))
+
+    def train(self):
+        temp = []
+        equal = False
+        iterations = 0
+        while(not equal) and iterations < 10000:
+            # store weight matrices to check for convergence
+            temp = []
+            for i in self.weight_matricies:
+                temp.append(np.copy(i))
+                
+            self.network_train_iteration()
+            # print("outputs\n",self.outputs)
+            print(self)
+            iterations +=1
+            equal = True
+            #check for convergence
+            for i in range(len(self.weight_matricies)):
+                if(not np.array_equal(self.weight_matricies[i].round(decimals = 4),temp[i])):
+                    equal = False
+            # print(equal)
+
+    def network_train_iteration(self):
+        # create variable to track error
+        self.errors = np.zeros(self.layers[-1].shape)
+
+        #create target vector to compare to output
+        if len(self.output_size) > 1:
+            self.target_vector = np.zeros((np.shape(self.layers[-1])))
+        else: 
+            self.target_vector = np.ones(1)
+        
+        # itereate through data and train
+        for d in self.data:
+            #reset target vector for each data point
+            self.target_vector = np.multiply(self.target_vector,0)
+            target_class = d[0]
+            if len(self.output_size) > 1:
+                self.target_vector[(int(target_class)-1)] = 1
+            else: 
+                self.target_vector = target_class
+
+            #set inputs
+            inputs = np.vstack(d[1:])
+
+            # feed data point through network
+            self.feed_forward(inputs)
+            
+            #calculate error on the output
+            self.error = np.subtract(self.layers[-1], self.target_vector)
+
+            # backpropogate error to train
+            self.back_propogation_single()
+            
+    def feed_forward(self, inputs):
+        self.layers[0] = inputs
+
+        for i in range(len(self.weight_matricies)):
+            #check if regression problem so we dont sigmodify the final output
+            if self.layers[-1].shape == self.layers[i+1].shape and len(self.output_size) == 1:
+                self.layers[i+1] = np.dot(self.weight_matricies[i], self.layers[i])
+            else:
+                self.layers[i+1] = np.dot(self.weight_matricies[i], self.layers[i])
+                #sigmoid calculation
+                exp = np.exp(-self.layers[i+1])
+                exp_1 = np.add(exp, 1)
+                self.layers[i+1] = 1/exp_1
+        # print("output", self.layers[-1])
+        return self.layers[-1]
+
+    def back_propogation_single(self):
+        i = len(self.weight_matricies)
+        #initialize next_error to current error
+        next_error = self.error
+        while i > 0: #iterate back through layers
+            regularizer = np.multiply(self.layers[i], np.subtract(1,self.layers[i]))
+            regularizer_error = np.multiply(next_error, regularizer)
+            delta_WM = self.learing_rate * np.dot(regularizer_error, np.transpose(self.layers[i-1]))
+            next_error = np.dot(np.transpose(self.weight_matricies[i-1]), next_error)
+            if self.momentum == True:
+                delta_WM_momentum = delta_WM + np.multiply(self.previous_WM_delta[i-1],self.momentum_factor)
+                self.previous_WM_delta[i-1] = delta_WM
+                self.weight_matricies[i-1] -= delta_WM_momentum
+                
+            else:
+                self.weight_matricies[i-1] -= delta_WM
+
+            i -= 1
+
+    def __str__(self):
+        stringify = "NETWORK:"
+        stringify+= "\nINPUT:\n"+str(self.layers[0])
+        # for i in range(len(self.weight_matricies)):
+            stringify+= "\nWEIGHTS:\n"+str(self.weight_matricies[i])
+        stringify += "\nOUTPUT\n"+str(self.layers[-1])
+        stringify += "\nEND NETWORK"
+        return stringify
+
+    def classify(self, test_data):
+        #run test data and return combination of predicted and target/actual class
+        tuples = []
+        for i in test_data:
+            target = np.array(i[0])
+            inputs = np.vstack(i[1:])
+            outputs = self.feed_forward(inputs)
+            guess = np.argmax(outputs)
+            tuples.append([target, guess])
+        return tuples
---
+++ Assignment_3_V2/RBN_clean.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+class RBN():
+    #input-> #data_as_2dList, possible_outputs, number_of_hidden_nodes
+    def __init__(self, data, output, gaussian_function_type, centers):
+        
+        self.data = np.array(data)
+        self.learning_rate = 0.01
+        self.number_of_nodes = len(centers)
+        self.centers = np.array(centers)
+        self.rbf_layer = np.random.rand(1,len(centers))
+        self.outputs = np.zeros(len(output))
+        self.outputs.shape = (1,len(output))
+        self.weight_matrix = np.random.rand(len(output), len(centers))
+        self.weight_matrix /= len(centers)**2
+        # find max distance for use in stdev calc
+        self.d_max = self.distance_max(self.centers)
+        #calculate stdev of hidden nodes
+        self.stdev = float(self.d_max) / ((2 * len(self.centers))**0.5)
+        self.errors = np.zeros(self.outputs.shape)
+
+    def __str__(self):
+        stringify = "Weights:\n"
+        stringify += str(self.weight_matrix)
+        stringify += "\nOutputs\n"
+        stringify += str(self.outputs)
+        return stringify
+    
+    def train(self):
+        temp = []
+        equal = False
+        iterations = 0
+        while(not equal) and iterations < 50:
+            # store weight matrices to check for convergence
+            tempWM = np.copy(self.weight_matrix)
+            self.network_train_iteration()
+            print(self)
+            iterations +=1
+            print("Iteration ", iterations)
+            equal = True
+
+            # check for convergence
+            if(not np.array_equal(self.weight_matrix, tempWM)):
+                    equal = False
+
+    def network_train_iteration(self):            
+        #for every data point (vector)
+        for d in self.data:
+            self.target = d[0] #first index is the class
+            inputs = np.stack(d[1:])
+            centers_no_class = self.centers[:,1:]
+
+            # run data point through network
+            self.feed_forward(inputs, centers_no_class)
+
+            # calculate errors
+            if len(self.outputs) == 1:  #regression problem
+                self.errors = (self.target - self.outputs)
+
+            else:  #classification
+                self.target_vector = np.zeros((self.outputs.shape))
+                self.target_vector[(int(self.target)-1)] = 1 #ex: target = 3 -->   [0, 0, 1]
+                self.errors = np.subtract(self.target_vector, self.outputs) #np array
+
+            #backpropogate error through and train
+            self.backprop()
+
+
+    def feed_forward(self, inputs, centers_no_class):
+        #### Calculate node values
+        # calculate attribute distances from centroids/nodes
+        deltas = np.subtract(inputs, centers_no_class)
+        deltas_squared = np.square(deltas)
+        exp_inside = np.divide(deltas_squared, (-2 * self.stdev**2))
+        sum_exp_inside = np.zeros((len(exp_inside),1))
+        for i in range(len(exp_inside)):
+            sum_exp_inside[i,0] = np.sum(exp_inside[i])
+        exp = np.exp(sum_exp_inside)
+        self.rbf_layer = exp
+
+        #now multiply RBF layer by weights to get output
+        self.outputs = np.dot(self.weight_matrix, self.rbf_layer)
+        self.outputs = np.divide(self.outputs, self.number_of_nodes)
+        
+    ###calculates max distance between the hidden layer nodes for used in stdev calc
+    def distance_max(self, centers):
+        dist_max = 0
+        for i in range(len(centers)-1):
+            j = i + 1
+            while j < len(centers):
+                distance = np.sum((centers[i]-centers[j])**2)
+                if distance > dist_max:
+                    dist_max = distance
+                j+=1
+        return dist_max
+
+    def backprop(self):
+        #update WM
+        delta_WM = self.learning_rate * np.dot(self.errors, np.transpose(self.rbf_layer))
+        self.weight_matrix += delta_WM
+
+
+    #takes a list of training points and returns a list of tuples corresponding to them (actual class/value, guessed class/value)
+    ## for use in F-Score and Regession calculations
+    def classify(self, test_data):
+        centers_no_class = self.centers[:,1:]
+        center_classes = self.centers[:,0]
+        guesses = []
+        for i in test_data:
+            target = i[0]
+            data = i[1:]
+            self.feed_forward(data, centers_no_class)
+            output = self.outputs
+            if(len(out)==1):#regression
+                output = out[0]
+            else:#classification
+                output = np.argmax(self.outputs[0])+1 #index of max (self.outputs)
+    
+            guesses.append([target,output])
+        return guesses
+        
