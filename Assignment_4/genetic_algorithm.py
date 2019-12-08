bcc2b64d49e677c7cebd39cb1249e289ba19bb42
BruceClark406 <brucewestonmt@gmail.com>
2019-12-08 12:23:42 -0700

    updating

--- Assignment_4/genetic_algorithm.py
+++ Assignment_4/genetic_algorithm.py
@@ -1,6 +1,7 @@
 from population_manager import PopulationManager
 from data_processing import Data_Processing
 import random
+import math
 
 
 class Genetic_Algorithm(PopulationManager):
@@ -10,6 +11,7 @@ class Genetic_Algorithm(PopulationManager):
         self.training_data = training_data
         self.number_of_inputs = mlp_dims[1]
         self.number_of_outputs = mlp_dims[-1]
+        self.gen_replacement_rate = .5
 
         
     #calculates a fitness for each indiv. in pop.
@@ -26,11 +28,8 @@ class Genetic_Algorithm(PopulationManager):
             i.fitness(inputs, outputs)
     
     
-
+    #picks an indiv from the pop. with highly fit indiv more likley to be chosen
     def russian_wheel_selection(self):
-        #add up all the fitness
-        #randomly select a number btw 0-fitness sum
-        #select which ever individual it lands on
         
         #sum up the fitness
         fitness_sum = 0
@@ -39,33 +38,49 @@ class Genetic_Algorithm(PopulationManager):
         
         #pick a radom float between 0 and fitness sum
         selection = random.uniform(0, fitness_sum)
+
         #when the fitness sum is bigger or equal to the random number, select individ
         fitness_sum = 0
-        for i in self.population:
-            fitness_sum += i.individual_fitness
+        for i in range(len(self.population)):
+            fitness_sum += self.population[i].individual_fitness
             if selection <= fitness_sum:
                 return i
         
 
-
-    def cross_over(self):
-        pass
-
     def run_genetic_algorithm(self):
 
-        self.calculate_fitness()
-        individual1 = self.russian_wheel_selection()
-        individual2 = self.russian_wheel_selection()
-        while individual1.layers[0] == individual2.layers[0]:
-            individual2 = self.russian_wheel_selection()
-        
-        child = self.uniform_cross(individual1, individual2)
-        self.mutation(child)
-        
-        
-        
-    
 
+        number_to_replace = int(len(self.population) * self.gen_replacement_rate)
+
+        number_of_generations = 100
+
+        for j in range(number_of_generations):
+
+            self.calculate_fitness()
+            count = 0
+            for k in self.population:
+                print(count, "---", k.individual_fitness)
+                count+=1
+
+            for i in range(math.ceil(number_to_replace/2)):
+                #select
+                individual1 = self.russian_wheel_selection()
+                individual2 = self.russian_wheel_selection()
+                while individual1 == individual2:
+                    individual2 = self.russian_wheel_selection()
+
+                #cross-over
+                child1, child2 = self.uniform_cross(self.population[individual1], self.population[individual2])
+
+                #mutation
+                child1.rezip_neuron(self.mutation(child1))
+                child2.rezip_neuron(self.mutation(child2))
+
+                #replace selected old gen. with new gen.
+                self.population[individual1] = child1
+                self.population[individual2] = child2
+
+            
 
 
 if __name__ == "__main__":
